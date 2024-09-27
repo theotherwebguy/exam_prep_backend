9dae069d1dd572967cf1755794d9ea45d9a8f9f7
MSNDLELA <msizi.ndlela5@gmail.com>
2024-09-27 10:51:05 +0200

    initial commit

---
+++ src/main/java/org/backend/examprep_backend/ExamprepBackendApplication.java
@@ -0,0 +1,13 @@
+package org.backend.examprep_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamprepBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamprepBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/backend/examprep_backend/ExamprepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.examprep_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamprepBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
